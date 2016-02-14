9c81bd62bf27c90fa390e76a6040f5768b6dc3aa
Tomaž Tekavec <tomaz@codurance.com>
2016-02-14 22:51:42 +0000

    First test case added.

---
+++ SeriesXAPI.Tests/SeriesXShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using NUnit.Framework;
+
+namespace SeriesXAPI.Tests
+{
+    [TestFixture]
+    public class SeriesXShould
+    {
+
+        [TestCase(3,1,3)]
+        public void return_nth_element_value_which_is_divisible_by_a_given_number_at_nth_position(long divisor, long position, long value)
+        {
+            var seriesX = new SeriesX();
+            var result = seriesX.GetElementValue(new BigInteger(divisor), new BigInteger(position));
+
+            Assert.AreEqual(new BigInteger(value), result);
+        }
+
+    }
+}
---
+++ SeriesXAPI/SeriesX.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace SeriesXAPI
+{
+    public class SeriesX
+    {
+        public BigInteger GetElementValue(BigInteger divisor, BigInteger position)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
